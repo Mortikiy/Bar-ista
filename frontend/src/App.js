b3c50082759645ecba3349195eaab0751f13458a
Sam <thelaserblade@gmail.com>
2023-03-21 19:16:29 -0400

    start

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+import LoginPage from './pages/LoginPage';
+function App() {
+  return (
+  <LoginPage/>  
+);
+}
+
+export default App;
---
+++ frontend/src/components/PageTitle.js
@@ -0,0 +1,8 @@
+import React from 'react';
+function PageTitle()
+{
+   return(
+     <h1 id="title">Bartender</h1>
+   );
+};
+export default PageTitle;
