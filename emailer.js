e21180099889b1c2b741dfbdce317722e9bde898
Mortikiy <114204417+Mortikiy@users.noreply.github.com>
2023-03-27 01:44:20 -0400

    Update emailer.js

--- emailer.js
+++ emailer.js
@@ -14,9 +14,9 @@ exports.confirmationEmail = async function(user){
     //console.log(user._id.toString());
     let emailToken = await jwt.sign(userID, process.env.SECRET_TOKEN);
     //console.log(emailToken);
-    let url = `https://obscure-springs-89188.herokuapp.com/confirmation/${emailToken}`;
+    let url;
 
-    /*
+    
     if (process.env.NODE_ENV === 'production')
     {
         url = `https://obscure-springs-89188.herokuapp.com/confirmation/${emailToken}`;
@@ -25,7 +25,7 @@ exports.confirmationEmail = async function(user){
     {
         url = `http://localhost:5000/confirmation/${emailToken}`;
     }
-    */
+    
 
     //console.log(user.email);
 
